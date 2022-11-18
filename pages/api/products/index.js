ad46e7e20dd386351213ec5cef7241f28a8a8ba4
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-11-18 10:38:43 +0100

    add put request

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,18 +1,38 @@
-import { products } from "../_data/products";
+import connectDB from "../_db/connect-db";
+import { Product } from "../_db/models/Product";
 
-export default function handler(req, res) {
+async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      const filteredProducts = req.query.category
-        ? products.filter((product) => product.category === req.query.category)
-        : products;
-      return res.status(200).json(
-        filteredProducts.map((p) => {
-          const { detail, ...rest } = p;
-          return rest;
-        })
-      );
+      try {
+        const filter = req.query;
+        console.log(filter);
+        const los = JSON.parse(filter.category);
+        const products = await Product.find({ category: los });
+        res.status(200).json(products);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+
+      break;
+    case "POST":
+      try {
+        /*  const product = new Product(body); */
+        const newProduct = new Product({
+          name: req.body.name,
+          category: req.body.category,
+          detail: req.body.detail,
+        });
+        await newProduct.save();
+        res.status(200).json();
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+      break;
+
     default:
       return res.status(400).json({ error: "method not supported" });
   }
 }
+
+export default connectDB(handler);
